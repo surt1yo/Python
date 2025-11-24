0b684eabf3bfeff09ca6b37cc5de65006867518b
surt1yo <mezaan.rizv@gmail.com>
2025-11-24 18:37:59 +1300

    initial commit

---
+++ list comprehension/main2.py
@@ -0,0 +1,8 @@
+#Write a program where the value of i begins from 1 and goes to 10. 
+#When the value of i becomes 5, 
+#force the interpreter to exit the program.\
+for i  in range(1,10):
+    if i==5:
+        exit()
+    else:
+        print(i)    
