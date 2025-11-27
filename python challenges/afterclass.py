33591ffadb752dd6b74533e399b4d995728a21da
surt1yo <mezaan.rizv@gmail.com>
2025-11-27 15:54:59 +1300

    initial commit

---
+++ python challenges/afterclass.py
@@ -0,0 +1,18 @@
+import random
+import string
+def generate_password(length=12):
+    lowercase = string.ascii_lowercase
+    uppercase = string.ascii_uppercase
+    digits = string.digits
+    password_chars = [
+        random.choice(lowercase),
+        random.choice(uppercase),
+        random.choice(digits)
+    ]
+    all_chars = lowercase + uppercase + digits
+    for _ in range(length - len(password_chars)):
+        password_chars.append(random.choice(all_chars))
+    random.shuffle(password_chars)
+    password = ''.join(password_chars)
+    return password
+print(generate_password(12))
