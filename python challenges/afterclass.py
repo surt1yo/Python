64926982f09d449d75fd7a0500291631da0324a2
surt1yo <mezaan.rizv@gmail.com>
2025-11-28 18:35:24 +1300

    initial commit

--- python challenges/afterclass.py
+++ python challenges/afterclass.py
@@ -4,12 +4,14 @@ def generate_password(length=12):
     lowercase = string.ascii_lowercase
     uppercase = string.ascii_uppercase
     digits = string.digits
+    symbols= string.punctuation
     password_chars = [
         random.choice(lowercase),
         random.choice(uppercase),
-        random.choice(digits)
+        random.choice(digits),
+        random.choice(symbols)
     ]
-    all_chars = lowercase + uppercase + digits
+    all_chars = lowercase + uppercase + digits + symbols
     for _ in range(length - len(password_chars)):
         password_chars.append(random.choice(all_chars))
     random.shuffle(password_chars)
@@ -17,7 +19,7 @@ def generate_password(length=12):
     return password
 if __name__ == "__main__":
     while True:
-        pwd = generate_password(12)
+        pwd = generate_password()
         print("Password:", pwd)
         choice = input('Type "new" for a new password or "q" to quit: ').strip().lower()
         if choice != "new":
