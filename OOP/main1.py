ba1be8820fa8bbb645dbf369ecd8dfe4746eb3d9
surt1yo <mezaan.rizv@gmail.com>
2025-11-28 18:23:18 +1300

    initial commit

--- OOP/main1.py
+++ OOP/main1.py
@@ -2,3 +2,11 @@
 #1. Create an __init__ method with arguments - max_speed and mileage 
 #2. Create an object of class Vehicle and pass the maximum speed and mileage of the car 
 #3. Print the values of max_speed and mileage by using the object
+class Vehicle:
+    def __init__(self,max_speed,mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+BMW=Vehicle(240,20)
+Mercedes=Vehicle(200,25) 
+print(f"The max speed of BMW is {BMW.max_speed}km/h and the mileage is {BMW.mileage}km/l")       
+print(f"The max speed of Mercedes is {Mercedes.max_speed}km/h and the mileage is {Mercedes.mileage}km/l")       
