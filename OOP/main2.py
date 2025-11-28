b1c8f73f7b3558173b261b9ad3dae32b5f10be4a
surt1yo <mezaan.rizv@gmail.com>
2025-11-28 18:28:02 +1300

    initial commit

--- OOP/main2.py
+++ OOP/main2.py
@@ -4,3 +4,12 @@
 #3. Create instances of class Parrot, passing arguments as well 
 #4. Print Class variable by accessing it 
 #5. Print Instance variables as well
+class Parrot:
+    species="Bird"
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+parrot=Parrot("Harry",3)
+parrot1=Parrot("Bob",4)
+print(f"{parrot.name} is a {parrot.species} and is {parrot.age} years old.")
+print(f"{parrot1.name} is a {parrot1.species} and is {parrot1.age} years old.")
