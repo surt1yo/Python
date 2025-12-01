839215b543b46bf67d2de3b6f36489d43c07c582
surt1yo <mezaan.rizv@gmail.com>
2025-12-01 18:08:25 +1300

    initial commit

--- OOP/OOP2/main.py
+++ OOP/OOP2/main.py
@@ -1,3 +1,8 @@
+#Write a program to create a class IOString which consists of a 
+#constructor that gives a default value to variable str1.
+#Next up create a method that gets a string as input from the user. 
+#Create another method that will print the string in the upper case. 
+#Next up create an object and call methods to get everything implemented.
 class IOstring:
     def __init__(self):
         print("constructor initializer")
