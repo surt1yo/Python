fc94e8cc1dcf35d0c099d383429c599b9cdf5938
surt1yo <mezaan.rizv@gmail.com>
2025-12-09 18:37:18 +1300

    initial commit

--- OOP/OOP5/afterclass.py
+++ OOP/OOP5/afterclass.py
@@ -1,3 +1,24 @@
 #Write a Python program to create two classes - 
 #BMW and Ferrari with similar methods and implement 
 #polymorphismPolymorphism on them.
+class BMW:
+    def origin(self):
+        print("BMW is a German car manufacturer.")
+    def maxspeed(self):
+        print("The maximum speed of BMW cars is around 300 k/h.")
+    def price(self):
+        print("The average price of a BMW is around $100,000.\n--------------------------------")
+class Ferrari:
+    def origin(self):
+        print("Ferrari is an Italian car manufacturer.")
+    def maxspeed(self):
+        print("The maximum speed of Ferrari cars is around 340 k/h.")
+    def price(self):
+        print("The average price of a Ferrari is around $250,000.")
+bmw = BMW()
+ferrari = Ferrari()
+for car in (bmw,ferrari):
+    car.origin()
+    car.maxspeed()
+    car.price()
+    
