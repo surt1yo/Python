94bce98d6c73715883a1a3228e6b57079031f0e5
surt1yo <mezaan.rizv@gmail.com>
2025-12-10 18:18:25 +1300

    initial commit

---
+++ OOP/OOP6/main1.py
@@ -0,0 +1,9 @@
+#Create a python program to create a flashcard using object-oriented programming. 
+#A flashcard is a two-sided card with information on both sides that can assist memory.
+# A question and an answer are usually printed on one side of a flashcard. 
+#Follow these steps to complete the activity - 
+#1. From the user, take the input for a word and its definition. 
+#2. To assign values for Word and Meaning, create a class called flashcard 
+#and use the __init__() function. 3. We'll use the __str__() function to get a 
+#string with the term and its definition. 4. Save the strings that have been returned in a list. 
+#5. To print all of the stored flashcards, use a while loop.
