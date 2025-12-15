f447af376409f6da8c17a64d84f1f792d8fcaa2b
surt1yo <mezaan.rizv@gmail.com>
2025-12-15 17:57:57 +1300

    initial commit

--- pygame/pygame1/afterclass.py
+++ pygame/pygame1/afterclass.py
@@ -1,8 +1,8 @@
 #Write a Python program to create your first Game screen using the Pygame library.
 import pygame
-# Initialize Pygame
+#Initialize Pygame
 pygame.init()
-# Create the game window
+#Create the game window
 screen=pygame.display.set_mode((500, 300))
 image=pygame.image.load('bigguy.jpg')
 bigguy_img=(pygame.transform.scale(image,(300,300)))
@@ -10,8 +10,7 @@ bg_color = (58,58,58)
 screen.fill(bg_color)
 screen.blit(bigguy_img,(100,0))
 pygame.display.flip()
-
-# Main loop to keep the window open
+#Main loop to keep the window open
 done = False
 while not done:
     for event in pygame.event.get():
