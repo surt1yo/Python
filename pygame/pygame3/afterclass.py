52722f77edc67113ee5c498e01550b442d251134
surt1yo <mezaan.rizv@gmail.com>
2025-12-17 18:48:56 +1300

    initial commit

---
+++ pygame/pygame3/afterclass.py
@@ -0,0 +1,3 @@
+#Write a Python program to create a Game screen and add two rectangular 
+#sprites to it using the Pygame library. Add controls to one of the sprites 
+#as well for up, down, left and right movements.
