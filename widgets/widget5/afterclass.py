9bbdbd8b0504e0e2ea6d477bce5e01279788516b
surt1yo <mezaan.rizv@gmail.com>
2026-01-28 18:44:25 +1300

    initial commit

---
+++ widgets/widget5/afterclass.py
@@ -0,0 +1,4 @@
+# Write a Python program to create an application that 
+# accepts a password from the user and displays its 
+# strength based on the length of the password using 
+# the Python Tkinter library.
---
+++ widgets/widget5/main1.py
@@ -0,0 +1,5 @@
+# Write a Python program to create a Denominator 
+# Calculator to calculate the number of notes 
+# of denominations - 2000, 500, and 100 for 
+# the amount entered by the user. And create 
+# its GUI for the user by using the Tkinter library.
