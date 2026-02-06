f485836e67e244d15308c40bc8be9673af1933d6
surt1yo <mezaan.rizv@gmail.com>
2026-02-06 18:41:39 +1300

    initial commit

--- course-2/intro/intro1/afterclass.py
+++ course-2/intro/intro1/afterclass.py
@@ -1,11 +1,10 @@
-# Create a basic Python program that simulates an AI chatbot, 
-# interacting with users through text and responding 
-# intelligently based on their input.
+# Improve the hello AI chatbot
+
 print("Hello! I am your AI chatbot. What is your name?")
 name = input()
 print(f"Nice to meet you, {name}, how are you feeling today?")
 answer = input()
-if answer.lower() in ["good", "great", "fine", "well","okay","ok"]:
+if answer.lower() in ["good", "great", "fine", "well"]:
     print("Nice to hear that!")
 else:
     print("I'm sorry to hear that.")
