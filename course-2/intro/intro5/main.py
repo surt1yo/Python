70f36b5a62c72b8c58dd12a039e642c48e9f6ac8
surt1yo <mezaan.rizv@gmail.com>
2026-02-15 10:58:28 +1300

    initial commit

--- course-2/intro/intro5/main.py
+++ course-2/intro/intro5/main.py
@@ -16,7 +16,8 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 model = models.Sequential([
     layers.Input(shape=(28, 28)),       
     layers.Flatten(),
-    layers.Dense(128, activation='relu')
+    layers.Dense(128, activation='relu'),
+    layers.Dense(10, activation='softmax')  # Output layer for 10 digit classes
 ])
 
 
