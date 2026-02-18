f111ca5e99f073e5cc03e4c6c92596b9f745b616
surt1yo <mezaan.rizv@gmail.com>
2026-02-18 18:18:43 +1300

    initial commit

---
+++ course-2/opencv/opencv1/main1.py
@@ -0,0 +1,4 @@
+# In this activity, students will load an image, 
+# convert it to grayscale, resize it to a standard 
+# size (224x224), display the processed image, 
+# and optionally save it based on key press.
