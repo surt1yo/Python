1082ffbfdbc1193878abfa54d603273966ab74c9
surt1yo <mezaan.rizv@gmail.com>
2026-02-20 18:59:08 +1300

    initial commit

---
+++ course-2/opencv/opencv1/afterclass.py
@@ -0,0 +1,22 @@
+# Create a Python script using OpenCV to 
+# resize an image into three predefined 
+# sizes, display each resized image, and save it.
+import cv2
+print(cv2.__version__)
+
+# Read the image
+img = cv2.imread("course-2/opencv/opencv1/monk.png")
+if img is None:
+    print("Could Not Read Image")
+else:
+    print("Image loaded successfully")
+    cv2.namedWindow("Monk Image", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Monk Image", 600, 400)
+    # Convert to Grayscale
+    grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Monk Image", grey_img)
+    key = cv2.waitKey(0)
+    if key == ord("s"):
+        cv2.imwrite("course-2/opencv/opencv1/monk_gray.png", grey_img)
+        print("Image saved as monk_gray.png.")
+    cv2.destroyAllWindows()
--- course-2/opencv/opencv2/afterclass.py
+++ course-2/opencv/opencv2/afterclass.py
@@ -1,3 +1,30 @@
 # Learn how to apply basic image manipulation techniques 
 # like rotation, cropping, and brightness adjustments, 
 # while also gaining experience with image transformations.
+import cv2
+import matplotlib.pyplot as plt
+
+
+image=cv2.imread("course-2\opencv\opencv1\monk.png")
+image_rgb=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+h,w=image.shape[:2] #return h and w but the first value is width
+c=(w//2,h//2)
+print(c)
+m=cv2.getRotationMatrix2D(c,45,1)
+# Rotate the image by 45 degrees
+rotated_image=cv2.warpAffine(image,m,(w,h))
+
+r_rgb=cv2.cvtColor(rotated_image,cv2.COLOR_BGR2RGB)
+plt.imshow(r_rgb)
+plt.title("Rotated Image")
+plt.show()
+
+# Increase brightness by adding 50 to all pixel values
+# Use cv2.add to avoid negative values or overflow
+brightness_matrix = np.ones(image.shape, dtype="uint8") * 50
+brighter = cv2.add(image, brightness_matrix)
+
+brighter_rgb = cv2.cvtColor(brighter, cv2.COLOR_BGR2RGB)
+plt.imshow(brighter_rgb)
+plt.title("Brighter Image")
+plt.show()
