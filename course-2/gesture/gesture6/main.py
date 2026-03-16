770ccdbb02789b49ee39b9a952a56e356ae39476
surt1yo <mezaan.rizv@gmail.com>
2026-03-16 17:46:48 +1300

    initial commit

--- course-2/gesture/gesture6/main.py
+++ course-2/gesture/gesture6/main.py
@@ -1,1 +1,5 @@
-
+# In this activity, students will build
+# a real-time gesture-controlled photo 
+# app that allows them to apply different 
+# filters (grayscale, sepia, negative, blur) 
+# and take screenshots using hand gestures.
