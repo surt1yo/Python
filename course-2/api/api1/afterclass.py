9daf4544e3670b762893f21a905dc1290cbfdbbe
surt1yo <mezaan.rizv@gmail.com>
2026-03-17 18:23:07 +1300

    initial commit

---
+++ course-2/api/api1/afterclass.py
@@ -0,0 +1,8 @@
+# In this assignment, you will practice 
+# fetching data from public APIs using
+# Python's requests library. You will
+# work with JSON data, extract useful 
+# information, and display it. The goal 
+# is to solidify your understanding of 
+# how APIs work and how to interact
+# with them effectively.
---
+++ course-2/gesture/gesture6/afterclass.py
@@ -0,0 +1,8 @@
+# In this assignment, you will enhance your 
+# gesture-controlled photo app by experimenting 
+# with different hand gestures to capture images
+# and apply dynamic filters (grayscale, sepia, negative, blur). 
+# You will implement the logic to switch between different 
+# filters based on hand gestures, such as thumb touching 
+# the index finger for photo capture and thumb 
+# touching any other finger for changing filters.
