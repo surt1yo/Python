7400691679fcd6d8c87a00ad41093a25576fbe70
surt1yo <mezaan.rizv@gmail.com>
2026-03-24 18:28:05 +1300

    Delete course-2/course-2 directory

--- course-2/course-2/api/api2/afterclass.py
+++
@@ -1,6 +0,0 @@
-# In this assignment, you will continue 
-# working with APIs to fetch trivia questions 
-# from a public API and create a basic interactive 
-# quiz application. The goal is to learn how to 
-# fetch data from APIs, handle JSON responses, 
-# and display the results in a user-friendly manner.
