03302f3325be1831ddaa31350c3edfeb9cddf796
surt1yo <mezaan.rizv@gmail.com>
2026-05-08 17:47:52 +1200

    initial commit

---
+++ course-2/prompt/prompt1/afterclass.py
@@ -0,0 +1,22 @@
+"""
+    To reinforce the skill of prompt 
+    engineering by crafting, refining, 
+    and evaluating prompts for clarity,
+    specificity, and context.
+                                        """
+from hf import generate_response
+
+print("Welcome to ai engineering tutorial!")
+vague = input("Enter a vague prompt: ")
+print("\nGenerating response for vague prompt...")
+print(generate_response(vague))
+
+
+specific = input("\nNow, enter a more specific prompt: ")
+print("\nGenerating response for specific prompt...")
+print(generate_response(specific))
+
+
+context = input("\nFinally, add some context to your prompt: ")
+print("\nGenerating response for prompt with context...")
+print(generate_response(context))
